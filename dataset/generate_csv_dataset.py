1e7f625e46d76c6f5e8075e364555dec54cbfaeb
Alexandr <iq98@list.ru>
2021-05-18 15:22:15 +0300

    added data generation

--- dataset/generate_csv_dataset.py
+++ dataset/generate_csv_dataset.py
@@ -1,7 +1,10 @@
 import argparse
+import random
 
 DEFAULT_DESCRIPTION = 'CSV dataset generator script demo.'
 DEFAULT_SAMPLES = 100
+DATA_SIZES = [100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000]
+DATA_SETS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
 
 
 def parse_args():
@@ -13,16 +16,23 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description=DEFAULT_DESCRIPTION)
 
-    parser.add_argument('output',
-                        type=str,
-                        help='output CSV file, e.g. data/output.csv')
+    # parser.add_argument('--output',
+    #                     type=str,
+    #                     default='data/output.csv',
+    #                     help='output CSV file, e.g. data/output.csv')
 
-    parser.add_argument('--samples',
+    parser.add_argument('samples',
                         type=int,
                         default=DEFAULT_SAMPLES,
                         help='number of samples to generate (default: {})'.format(DEFAULT_SAMPLES))
 
     return parser.parse_args()
+
+
+def generate_two_nodes(num_of_nodes):
+    node1 = random.randint(1, num_of_nodes-1)
+    node2 = random.randint(node1+1, num_of_nodes)
+    return node1, node2
 
 
 if __name__ == '__main__':
@@ -32,8 +42,13 @@ if __name__ == '__main__':
     if args.samples < 0:
         raise ValueError('Number of samples must be greater than 0.')
 
-    # запись данных в файл
-    with open(args.output, 'w') as file:
-        for i in range(args.samples - 1):
-            file.write('{},'.format(i))
-        file.write(str(args.samples - 1))
+    num_of_nodes = args.samples/10
+
+    for dataset in DATA_SETS:
+        # запись данных в файл
+        with open('data/'+dataset+'/'+str(args.samples)+'.csv', 'w') as file:
+            for i in range(args.samples):
+                node1, node2 = generate_two_nodes(num_of_nodes)
+                weight = random.randint(1, 20)
+                file.write(f'{node1};{node2};{weight};\n')
+
